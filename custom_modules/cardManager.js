a69ebf37888c9e568bf967f47da9f8e7f7325f8d
Nico Thuniot <68270525+Nico-Src@users.noreply.github.com>
2024-07-28 17:58:48 +0200

    Card Data now in seperate file

--- custom_modules/cardManager.js
+++ custom_modules/cardManager.js
@@ -2,6 +2,7 @@
 import { BoxGeometry, Vector3, MeshBasicMaterial, Mesh } from 'three';
 import { CARD_CONFIG } from './config';
 import { TEXTURE_MANAGER } from './textureManager';
+import { CARD_DATA } from './data';
 
 export default class CardManager{
     constructor(){
@@ -12,60 +13,32 @@ export default class CardManager{
         this.cards = [];
         this.geometry = new BoxGeometry(CARD_CONFIG.size.width, CARD_CONFIG.size.height, CARD_CONFIG.size.depth);
 
-        this.positions = [
-            new Vector3(-.5, 6.032, 3.81),
-            new Vector3(-.25, 6.017, 3.77),
-            new Vector3(0, 6.002, 3.75),
-            new Vector3(.25, 5.987, 3.77),
-            new Vector3(.5, 5.972, 3.81),
-        ];
-
-        this.rotations = [
-            new Vector3(-Math.PI / 2, 0, 0.15),
-            new Vector3(-Math.PI / 2, 0, 0.10),
-            new Vector3(-Math.PI / 2, 0, 0),
-            new Vector3(-Math.PI / 2, 0, -0.10),
-            new Vector3(-Math.PI / 2, 0, -0.15)
-        ];
-
-        this.cardData = [
-            {name: 'citizen1', path: '/citizen1.png'},
-            {name: 'citizen2', path: '/citizen2.png'},
-            {name: 'citizen3', path: '/citizen3.png'},
-            {name: 'citizen4', path: '/citizen4.png'},
-            {name: 'emperor', path: '/emperor.png'},
-            {name: 'slave', path: '/slave.png'},
-        ];
-
-        for(const card of this.cardData) TEXTURE_MANAGER.loadTexture(card.name, card.path);
+        for(const card of CARD_DATA.cards) TEXTURE_MANAGER.loadTexture(card.name, card.path);
         
         TEXTURE_MANAGER.loadTexture('cover', '/card2.png');
         TEXTURE_MANAGER.loadTexture('game_zone_edge', '/game_zone_edge.png');
         
-        this.edgeMaterial = new MeshBasicMaterial({color: 0x202020});
+        this.edgeMaterial = new MeshBasicMaterial({color: 0x202020, transparent: true});
 
-        for(let i = 0; i < this.cardData.length - 1; i++){
-            const card = this.cardData[i];
+        for(let i = 0; i < CARD_DATA.cards.length; i++){
+            const card = CARD_DATA.cards[i];
             const mat = [
                 this.edgeMaterial,
                 this.edgeMaterial,
                 this.edgeMaterial,
                 this.edgeMaterial,
-                new MeshBasicMaterial({map: TEXTURE_MANAGER.get(card.name)}),
-                new MeshBasicMaterial({map: TEXTURE_MANAGER.get('cover')})
+                new MeshBasicMaterial({map: TEXTURE_MANAGER.get(card.name), transparent: true, specular: 0x000000, emissive: 0x000000, reflectivity: 0.0}),
+                new MeshBasicMaterial({map: TEXTURE_MANAGER.get('cover'), transparent: true})
             ];
 
             const c = new Mesh(this.geometry, mat);
-            c.position.set(...this.positions[i]);
-            c.rotation.set(...this.rotations[i]);
-            this.configureCard(c, card.name, i);
+            this.configureCard(c, card.name);
         }
     }
 
-    configureCard(card, name, index) {
+    configureCard(card, name) {
         card.name = name;
         card.isCard = true;
-        card.inHandIndex = index;
         card.castShadow = true;
         this.cards.push(card);
     }
